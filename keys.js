2ce4a13eac611a21d0d45a76ab2bd5a4c382f857
thejasjanardhan <thejasj435@gmail.com>
2021-05-29 20:38:56 +0530

    bug fix

--- keys.js
+++ keys.js
@@ -1,5 +1,7 @@
 // API KEY - "https://developers.google.com/custom-search/v1/using_rest"
-export const API_KEY = "AIzaSyCQv0F9Fv5ckhXwT9_FgbWu-iTolZFSwM8";
+// export const API_KEY = "AIzaSyCQv0F9Fv5ckhXwT9_FgbWu-iTolZFSwM8";
+export const API_KEY = process.env.API_KEY;
 
 // Context Key - "https://cse.google.com/cse/create/new"
-export const CONTEXT_KEY = "5d7d933adf172be0c";
+// export const CONTEXT_KEY = "5d7d933adf172be0c";
+export const CONTEXT_KEY = process.env.CONTEXT_KEY;
